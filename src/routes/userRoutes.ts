0d9fa57d006510fa61840069b0992686458385b3
Nurul Amin <nurul.amin.naim@gmail.com>
2023-11-27 04:00:11 +1100

    order test done

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,5 +8,8 @@ router.get("/", Controllers.getAllUsers);
 router.get("/:userId", Controllers.getUserById);
 router.put("/:userId", Controllers.updateUser);
 router.delete("/:userId", Controllers.deleteUser);
+router.put("/:userId/orders", Controllers.addOrder);
+router.get("/:userId/orders", Controllers.getAllOrders);
+router.get("/:userId/orders/total-price", Controllers.calculateTotalPrice);
 
 export default router;
