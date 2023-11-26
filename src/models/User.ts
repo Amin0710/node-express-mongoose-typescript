20a14797bf1a530a0147ecede7054f06b2934759
Nurul Amin <nurul.amin.naim@gmail.com>
2023-11-27 02:49:00 +1100

    Interface done

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+	userId: number;
+	username: string;
+	password: string;
+	fullName: {
+		firstName: string;
+		lastName: string;
+	};
+	age: number;
+	email: string;
+	isActive: boolean;
+	hobbies: string[];
+	address: {
+		street: string;
+		city: string;
+		country: string;
+	};
+	orders: Array<{
+		productName: string;
+		price: number;
+		quantity: number;
+	}>;
+}
