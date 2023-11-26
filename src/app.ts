605c8294fdaaceb744fe6a6ff1859f001cebd640
Nurul Amin <nurul.amin.naim@gmail.com>
2023-11-27 02:43:51 +1100

    App.ts ready

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 
 const app: Application = express();
 const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!");
