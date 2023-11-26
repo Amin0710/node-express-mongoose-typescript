988ac8c9c6df2f78033169614f42880a0cbf4e4f
Nurul Amin <nurul.amin.naim@gmail.com>
2023-11-27 02:15:18 +1100

    1st

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
 const app: Application = express();
 const port = 3000;
 
@@ -7,3 +9,19 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 export default app;
+
+// import { StudentRoutes } from "./app/modules/student/student.route";
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use("/api/v1/students", StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+	const a = 10;
+	res.send(a);
+};
+
+app.get("/", getAController);
