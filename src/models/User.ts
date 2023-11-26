93d569676d5da50f4e35671d6751f55e2087ade6
Nurul Amin <nurul.amin.naim@gmail.com>
2023-11-27 02:50:28 +1100

    userSchema is ready

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import { Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
 	userId: number;
@@ -23,3 +23,33 @@ export interface IUser extends Document {
 		quantity: number;
 	}>;
 }
+
+const userSchema = new Schema<IUser>({
+	userId: { type: Number, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	fullName: {
+		firstName: { type: String, required: true },
+		lastName: { type: String, required: true },
+	},
+	age: { type: Number, required: true },
+	email: { type: String, required: true },
+	isActive: { type: Boolean, required: true },
+	hobbies: { type: [String], required: true },
+	address: {
+		street: { type: String, required: true },
+		city: { type: String, required: true },
+		country: { type: String, required: true },
+	},
+	orders: [
+		{
+			productName: { type: String, required: true },
+			price: { type: Number, required: true },
+			quantity: { type: Number, required: true },
+		},
+	],
+});
+
+const UserModel = mongoose.model<IUser>("User", userSchema);
+
+export default UserModel;
