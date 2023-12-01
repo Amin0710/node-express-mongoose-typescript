21736508699a58b7f8e32f6e568bae0db334d413
Nurul Amin <nurul.amin.naim@gmail.com>
2023-12-02 01:56:24 +1100

    .env remove

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-	res.send("Hello World!");
+	res.send("Hello World!!!!!!!!!!!!!!!!!!!!!");
 });
 
 export default app;
